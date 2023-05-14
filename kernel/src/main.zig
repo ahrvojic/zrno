0850051366c4b5f3177e6912daa28e10110111af
Anthony Hrvojic <anth@dezoris.net>
2023-05-13 22:00:17 -0400

    Initial import from limine-zig-template

---
+++ kernel/src/main.zig
@@ -0,0 +1,48 @@
+const limine = @import("limine");
+const std = @import("std");
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent. In Zig, `export var` is what we use.
+pub export var framebuffer_request: limine.FramebufferRequest = .{};
+
+// Set the base revision to 2, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+pub export var base_revision: limine.BaseRevision = .{ .revision = 2 };
+
+inline fn done() noreturn {
+    while (true) {
+        asm volatile ("hlt");
+    }
+}
+
+// The following will be our kernel's entry point.
+export fn _start() callconv(.C) noreturn {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (!base_revision.is_supported()) {
+        done();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response) |framebuffer_response| {
+        if (framebuffer_response.framebuffer_count < 1) {
+            done();
+        }
+
+        // Get the first framebuffer's information.
+        const framebuffer = framebuffer_response.framebuffers()[0];
+
+        for (0..100) |i| {
+            // Calculate the pixel offset using the framebuffer information we obtained above.
+            // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+            const pixel_offset = i * framebuffer.pitch + i * 4;
+
+            // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+            @as(*u32, @ptrCast(@alignCast(framebuffer.address + pixel_offset))).* = 0xFFFFFFFF;
+        }
+    }
+
+    // We're done, just hang...
+    done();
+}
