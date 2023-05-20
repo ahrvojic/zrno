3959cacc9f8d03fcac63d8e8cd22678a49aac0c1
Anthony Hrvojic <anth@dezoris.net>
2023-05-20 00:01:24 -0400

    Expand GDT implementation

--- kernel/src/arch/x86_64/gdt.zig
+++ kernel/src/arch/x86_64/gdt.zig
@@ -4,7 +4,7 @@ pub const USER_CODE   = 0x18;
 pub const USER_DATA   = 0x20;
 pub const TSS_DESC    = 0x28;
 
-const SegmentDesc = packed struct {
+const GDTDesc = packed struct {
     limit: u16,
     base_1: u16,
     base_2: u8,
@@ -14,7 +14,32 @@ const SegmentDesc = packed struct {
     zero: u32
 };
 
-const GDTDesc = packed struct {
+const GDTR = packed struct {
     limit: u16,
     base: *const GDTDesc
 };
+
+fn make_desc(limit: u16, base: u32, access: u8, granularity: u8) void {
+    return GDTDesc {
+        .limit = limit,
+        .base_1 = @truncate(u16, base),
+        .base_2 = @truncate(u8, base >> 16),
+        .base_3 = @truncate(u8, base >> 24),
+        .access = access,
+        .granularity = granularity
+    };
+}
+
+var gdt align(4) = []GDTDesc {
+    make_desc(0, 0, 0, 0),
+    // TODO
+};
+
+var gdtr = GDTR {
+    .limit = @as(u16, @sizeOf(@TypeOf(gdt)) - 1),
+    .base = &gdt[0]
+};
+
+pub fn init() void {
+    // TODO
+}
