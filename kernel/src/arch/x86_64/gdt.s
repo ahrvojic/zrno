ff62a0bb5165d45db199b79b69ca504f9f74096b
Anthony Hrvojic <anth@dezoris.net>
2023-06-27 14:36:40 -0400

    Load TSS

--- kernel/src/arch/x86_64/gdt.s
+++ kernel/src/arch/x86_64/gdt.s
@@ -2,18 +2,22 @@
 .type load_gdt, @function
 load_gdt:
     lgdt (gdtr)
-
 .reload_segments:
-    pushq $0x08 // kernel code segment
+    pushq kernel_code_seg
     leaq .reload_cs, %rax
     pushq %rax
     lretq
-
 .reload_cs:
-    movw $0x10, %ax // kernel data segment
+    movw kernel_data_seg, %ax
     movw %ax, %ds
     movw %ax, %es
     movw %ax, %fs
     movw %ax, %gs
     movw %ax, %ss
+    ret
+
+.global load_tss
+.type load_tss, @function
+load_tss:
+    ltr (tss_seg)
     ret
