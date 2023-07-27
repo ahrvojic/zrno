6477076564d48e24be9567d54185025582fcf3ec
Anthony Hrvojic <anth@dezoris.net>
2023-07-27 18:07:24 -0400

    Add serial port read/write support

---
+++ kernel/src/arch/x86_64/port.zig
@@ -0,0 +1,16 @@
+//! Port operations
+
+pub inline fn inb(port: u16) u8 {
+    return asm volatile (
+        \\inb %[port], %[res]
+        : [res] "={al}" (-> u8)
+        : [port] "N{dx}" (port));
+}
+
+pub inline fn outb(port: u16, value: u8) void {
+    asm volatile (
+        \\outb %[value], %[port]
+        :
+        : [value] "{al}" (value),
+          [port] "N{dx}" (port));
+}
