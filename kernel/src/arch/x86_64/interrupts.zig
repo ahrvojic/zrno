bc8cafe3b42b2c6947e5ef39bcda6bb3f44f96b4
Anthony Hrvojic <anth@dezoris.net>
2023-08-06 19:01:48 -0400

    Add interrupt handling (WIP)

---
+++ kernel/src/arch/x86_64/interrupts.zig
@@ -0,0 +1,75 @@
+//! Interrupt handlers
+
+const InterruptFrame = extern struct {
+    r15: u64,
+    r14: u64,
+    r13: u64,
+    r12: u64,
+    r11: u64,
+    r10: u64,
+    r9: u64,
+    r8: u64,
+    rsi: u64,
+    rdi: u64,
+    rbp: u64,
+    rdx: u64,
+    rcx: u64,
+    rbx: u64,
+    rax: u64,
+
+    vector_number: u64,
+    error_code: u64,
+
+    iret_rip: u64,
+    iret_cs: u64,
+    iret_flags: u64,
+    iret_rsp: u64,
+    iret_ss: u64,
+};
+
+export fn interruptDispatch(frame: *InterruptFrame) callconv(.C) void {
+    // TODO
+}
+
+export fn interruptStub() callconv(.Naked) void {
+    asm volatile (
+        \\push %rax
+        \\push %rbx
+        \\push %rcx
+        \\push %rdx
+        \\push %rbp
+        \\push %rdi
+        \\push %rsi
+        \\push %r8
+        \\push %r9
+        \\push %r10
+        \\push %r11
+        \\push %r12
+        \\push %r13
+        \\push %r14
+        \\push %r15
+        \\
+        \\mov %rsp, %rdi
+        \\call interruptDispatch
+        \\mov %rax, %rsp
+        \\
+        \\pop %r15
+        \\pop %r14
+        \\pop %r13
+        \\pop %r12
+        \\pop %r11
+        \\pop %r10
+        \\pop %r9
+        \\pop %r8
+        \\pop %rsi
+        \\pop %rdi
+        \\pop %rbp
+        \\pop %rdx
+        \\pop %rcx
+        \\pop %rbx
+        \\pop %rax
+        \\
+        \\add $16, %rsp
+        \\iretq
+    );
+}
