d9808ee22662d60d3f97446e8209ecdfb67dcef3
Anthony Hrvojic <anth@dezoris.net>
2023-09-13 22:01:51 -0400

    Add ACPI tables

---
+++ kernel/src/arch/x86_64/acpi.zig
@@ -0,0 +1,25 @@
+//! ACPI tables
+
+pub const RSDP = packed struct {
+    signature: [8]u8,
+    checksum: u8,
+    oem_id: [6]u8,
+    revision: u8,
+    rsdt_addr: u32,
+    length: u32,
+    xsdt_addr: u64,
+    extended_checksum: u8,
+    reserved: [3]u8,
+};
+
+pub const RSDT = packed struct {
+    signature: [4]u8,
+    length: u32,
+    revision: u8,
+    checksum: u8,
+    oem_id: [6]u8,
+    oem_table_id: [8]u8,
+    oem_revision: u32,
+    creator_id: u32,
+    creator_revision: u32,
+};
