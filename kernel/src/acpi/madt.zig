4300fd02851cb6be9c95a313abc8296a6e89048b
Anthony Hrvojic <anth@dezoris.net>
2023-10-21 22:48:54 -0400

    Rename MADT fields struct

--- kernel/src/acpi/madt.zig
+++ kernel/src/acpi/madt.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const acpi = @import("acpi.zig");
 const debug = @import("../sys/debug.zig");
 
-const MADTFields = extern struct {
+const MADT = extern struct {
     local_controller_addr: u32 align(1),
     flags: u32 align(1),
 };
@@ -15,15 +15,15 @@ const MADTEntry = extern struct {
 
 pub fn init(sdt: *const acpi.SDT) !void {
     const madt_data = sdt.getData();
-    const madt_fields = std.mem.bytesAsValue(MADTFields, madt_data[0..8]);
-    if (madt_fields.flags & 0x1 == 0) {
+    const madt = std.mem.bytesAsValue(MADT, madt_data[0..8]);
+    if (madt.flags & 0x1 == 0) {
         debug.panic("System must be PC-AT compatible!");
     }
 
     const madt_entries = madt_data[8..];
     var offset: usize = 0;
     while (true) {
-        if (sdt.length - @sizeOf(acpi.SDT) - @sizeOf(MADTFields) - offset < 2) {
+        if (sdt.length - @sizeOf(acpi.SDT) - @sizeOf(MADT) - offset < 2) {
             break;
         }
 
