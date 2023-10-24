cc5ea34dda8d3ff992d261ea86f501f009a1398a
Anthony Hrvojic <anth@dezoris.net>
2023-10-23 21:17:44 -0400

    Add MADT APIC structs

--- kernel/src/acpi/madt.zig
+++ kernel/src/acpi/madt.zig
@@ -3,14 +3,20 @@ const std = @import("std");
 const acpi = @import("acpi.zig");
 const debug = @import("../sys/debug.zig");
 
-const MADT = extern struct {
+const Fields = extern struct {
     local_controller_addr: u32 align(1),
     flags: u32 align(1),
 };
 
-const MADTHeader = extern struct {
+const Header = extern struct {
     id: u8 align(1),
     length: u8 align(1),
+};
+
+const LocalApic = extern struct {
+    processor_id: u8 align(1),
+    apic_id: u8 align(1),
+    flags: u32 align(1),
 };
 
 const IOApic = extern struct {
@@ -20,21 +26,36 @@ const IOApic = extern struct {
     gsib: u32 align(1),
 };
 
+const IOApicISO = extern struct {
+    bus_source: u8 align(1),
+    irq_source: u8 align(1),
+    gsi: u32 align(1),
+    flags: u16 align(1),
+};
+
+const LocalApicNMI = extern struct {
+    processor_id: u8 align(1),
+    flags: u16 align(1),
+    lint: u8 align(1),
+};
+
 pub fn init(sdt: *const acpi.SDT) !void {
     const madt_data = sdt.getData();
-    const madt = std.mem.bytesAsValue(MADT, madt_data[0..8]);
-    if (madt.flags & 0x1 == 0) {
+    const fields = std.mem.bytesAsValue(Fields, madt_data[0..8]);
+    if (fields.flags & 0x1 == 0) {
         debug.panic("System must be PC-AT compatible!");
     }
 
     const madt_entries = madt_data[8..];
-    const header_size = @sizeOf(MADTHeader);
+    const header_size = @sizeOf(Header);
     var offset: usize = 0;
     while (madt_entries.len - offset >= header_size) {
-        const entry: *const MADTHeader = @ptrCast(madt_entries[offset..(offset + header_size)].ptr);
+        const entry: *const Header = @ptrCast(madt_entries[offset..(offset + header_size)]);
         switch (entry.id) {
             0 => {
                 debug.println("[MADT] Found local APIC");
+                const lapic: *const LocalApic = @ptrCast(madt_entries[(offset + header_size)..entry.length]);
+                _ = lapic; // TODO
             },
             1 => {
                 debug.println("[MADT] Found I/O APIC");
@@ -43,12 +64,16 @@ pub fn init(sdt: *const acpi.SDT) !void {
             },
             2 => {
                 debug.println("[MADT] Found I/O APIC interrupt source override");
+                //const io_apic_iso: *const IOApicISO = @ptrCast(madt_entries[(offset + header_size)..entry.length]);
+                //_ = io_apic_iso; // TODO
             },
             3 => {
                 debug.println("[MADT] Found I/O APIC NMI source");
             },
             4 => {
                 debug.println("[MADT] Found local APIC NMIs");
+                //const lapic_nmi: *const LocalApicNMI = @ptrCast(madt_entries[(offset + header_size)..entry.length]);
+                //_ = lapic_nmi; // TODO
             },
             5 => {
                 debug.println("[MADT] Found local APIC address override");
