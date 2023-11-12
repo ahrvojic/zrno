361d3a8420521022b2a513521d9b468a086c00bd
Anthony Hrvojic <anth@dezoris.net>
2023-11-12 14:45:44 -0500

    Pass bootloader responses to cpu module

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -7,6 +7,7 @@ const debug = @import("sys/debug.zig");
 
 pub export var hhdm_req: limine.HhdmRequest = .{};
 pub export var rsdp_req: limine.RsdpRequest = .{};
+pub export var smp_req: limine.SmpRequest = .{};
 
 export fn _start() callconv(.C) noreturn {
     main() catch {};
@@ -17,12 +18,13 @@ pub fn main() !void {
     // Get needed info from bootloader
     const hhdm_res = hhdm_req.response.?;
     const rsdp_res = rsdp_req.response.?;
+    const smp_res = smp_req.response.?;
 
     cpu.interrupts_disable();
     defer cpu.interrupts_enable();
 
     debug.println("[Main] Init CPU");
-    try cpu.init();
+    try cpu.init(hhdm_res, smp_res);
 
     debug.println("[Main] Init ACPI");
     try acpi.init(hhdm_res, rsdp_res);
