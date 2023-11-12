68da6172c9b7f999ab607a3d3e65abeba20b8a79
Anthony Hrvojic <anth@dezoris.net>
2023-11-12 14:23:24 -0500

    Read lapic base address from MSR; move functions into cpu module

--- kernel/src/sys/cpu.zig
+++ kernel/src/sys/cpu.zig
@@ -4,18 +4,61 @@ const debug = @import("debug.zig");
 const gdt = @import("gdt.zig");
 const idt = @import("idt.zig");
 
-pub const CPU = struct {
+const lapic_msr = 0x1b;
+
+const CPU = struct {
     gdt: gdt.GDT = .{},
     tss: gdt.TSS = .{},
     idt: idt.IDT = .{},
+    lapic_base: u64,
 };
 
 pub fn init() !void {
-    var instance: CPU = .{};
+    var instance: CPU = .{
+        .lapic_base = readMSR(lapic_msr),
+    };
 
     debug.println("[CPU] Load GDT");
     instance.gdt.load(&instance.tss);
 
     debug.println("[CPU] Load IDT");
     instance.idt.load();
+}
+
+pub fn interrupts_enable() callconv(.Inline) void {
+    asm volatile ("sti");
+}
+
+pub fn interrupts_disable() callconv(.Inline) void {
+    asm volatile ("cli");
+}
+
+pub fn halt() callconv(.Inline) noreturn {
+    while (true) {
+        asm volatile ("hlt");
+    }
+}
+
+fn readMSR(msr: u32) callconv(.Inline) u64 {
+    var high: u32 = undefined;
+    var low: u32 = undefined;
+
+    asm volatile (
+        \\rdmsr
+        : [_] "={eax}" (low),
+          [_] "={edx}" (high),
+        : [_] "{ecx}" (msr),
+    );
+
+    return (@as(u64, high) << 32) | @as(u64, low);
+}
+
+fn writeMSR(msr: u32, value: u64) callconv(.Inline) void {
+    asm volatile (
+        \\wrmsr
+        :
+        : [_] "{eax}" (value & 0xffffffff),
+          [_] "{edx}" (value >> 32),
+          [_] "{ecx}" (msr),
+    );
 }
