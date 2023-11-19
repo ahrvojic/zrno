55012245070f19f7c23ebba0a78acc68ada26c0f
Anthony Hrvojic <anth@dezoris.net>
2023-11-19 18:16:59 -0500

    Add I/O APIC support

--- kernel/src/dev/apic.zig
+++ kernel/src/dev/apic.zig
@@ -1,2 +1,27 @@
+const limine = @import("limine");
+
 const debug = @import("../sys/debug.zig");
 const madt = @import("../acpi/madt.zig");
+
+const IOApic = struct {
+    io_apic_base: u64,
+
+    fn read(self: *const IOApic, offset: u32) u32 {
+        @as(*volatile u32, @ptrFromInt(self.address)).* = offset;
+        return @as(*volatile u32, @ptrFromInt(self.address + 0x10)).*;
+    }
+
+    fn write(self: *const IOApic, offset: u32, value: u32) void {
+        @as(*volatile u32, @ptrFromInt(self.address)).* = offset;
+        @as(*volatile u32, @ptrFromInt(self.address + 0x10)).* = value;
+    }
+};
+
+pub fn init(hhdm_res: *limine.HhdmResponse) !void {
+    var io_apic: IOApic = .{
+        // Just take first I/O APIC entry as Q35 machine only has one
+        .io_apic_base = madt.io_apics[0].?.address + hhdm_res.offset,
+    };
+
+    _ = io_apic; // TODO
+}
