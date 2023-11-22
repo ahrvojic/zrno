23f56c9cde8a8c89ea938114ee728f0c1e01e7a1
Anthony Hrvojic <anth@dezoris.net>
2023-11-21 23:41:04 -0500

    Add IRQ routing support to I/O APIC

--- kernel/src/dev/apic.zig
+++ kernel/src/dev/apic.zig
@@ -6,11 +6,46 @@ const madt = @import("../acpi/madt.zig");
 var io_apic: IOApic = .{};
 
 const IOApic = struct {
-    io_apic_base: u64 = undefined,
+    address: u64 = undefined,
+    gsib: u64 = undefined,
 
     pub fn init(self: *IOApic, hhdm_offset: u64) void {
-        // Just take first I/O APIC entry as Q35 machine only has one
-        self.io_apic_base = madt.io_apics[0].?.address + hhdm_offset;
+        // QEMU Q35 machine only has one I/O APIC
+        const io_apic_entry = madt.io_apics[0].?;
+        self.address = io_apic_entry.address + hhdm_offset;
+        self.gsib = io_apic_entry.gsib;
+    }
+
+    pub fn routeIrq(self: *const IOApic, lapic_id: u32, vector: u8, irq: u8) void {
+        // Use interrupt source override if exists
+        for (madt.io_apic_isos) |iso_opt| {
+            if (iso_opt) |iso| {
+                if (iso.irq_source == irq) {
+                    self.route(lapic_id, vector, iso.gsi, iso.flags);
+                    return;
+                }
+            }
+        }
+
+        // Otherwise route IRQ directly
+        self.route(lapic_id, vector, irq, 0);
+    }
+
+    fn route(self: *const IOApic, lapic_id: u32, vector: u8, gsi: u32, flags: u16) void {
+        // Calculate offset to I/O redirection table entry:
+        // - Table starts at 0x10
+        // - Add entry distance from global system interrupt base
+        // - Two registers per entry
+        const offset = 0x10 + (gsi - self.gsib) * 2;
+
+        // Construct redirection entry value
+        // Flags: level-triggered (bit 15), active-low (bit 13)
+        const value: u64 = @as(u64, @intCast(vector))
+            | @as(u64, @intCast(flags & 0b1010)) << 12
+            | @as(u64, @intCast(lapic_id)) << 56;
+
+        self.write(offset + 0, @truncate(value));
+        self.write(offset + 1, @truncate(value >> 32));
     }
 
     fn read(self: *const IOApic, offset: u32) u32 {
@@ -26,4 +61,8 @@ const IOApic = struct {
 
 pub fn init(hhdm_res: *limine.HhdmResponse) !void {
     io_apic.init(hhdm_res.offset);
+}
+
+pub fn get() *const IOApic {
+    return &io_apic;
 }
