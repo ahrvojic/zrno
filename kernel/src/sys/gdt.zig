3ace1961c4d5dbcb8ad43d553652af5f6ad770a2
Anthony Hrvojic <anth@dezoris.net>
2023-11-22 19:18:54 -0500

    Fix GDT and IDT structs

--- kernel/src/sys/gdt.zig
+++ kernel/src/sys/gdt.zig
@@ -26,75 +26,63 @@ const tss_access = 0b10001001;
 const code_flags = 0b1010;
 const data_flags = 0b1100;
 
-const TSSEntry = packed struct {
-    limit_1: u16,
+const TSSEntry = extern struct {
+    limit: u16,
     base_1: u16,
     base_2: u8,
     access: u8,
-    limit_2: u4,
-    flags: u4,
+    granularity: u8,
     base_3: u8,
     base_4: u32,
     reserved: u32,
 
-    pub fn make(base: u64, limit: u32, access: u8, flags: u4) TSSEntry {
+    pub fn make(base: u64, access: u8) TSSEntry {
         return .{
             .base_1 = @truncate(base),
             .base_2 = @truncate(base >> 16),
             .base_3 = @truncate(base >> 24),
             .base_4 = @truncate(base >> 32),
-            .limit_1 = @truncate(limit),
-            .limit_2 = @truncate(limit >> 16),
+            .limit = @sizeOf(TSS) - 1,
             .access = access,
-            .flags = flags,
+            .granularity = 0,
             .reserved = 0,
         };
     }
 };
 
-pub const TSS = packed struct {
-    reserved_1: u32 = 0,
-    rsp0: u64 = 0,
-    rsp1: u64 = 0,
-    rsp2: u64 = 0,
-    reserved_2: u32 = 0,
-    reserved_3: u32 = 0,
-    ist1: u64 = 0,
-    ist2: u64 = 0,
-    ist3: u64 = 0,
-    ist4: u64 = 0,
-    ist5: u64 = 0,
-    ist6: u64 = 0,
-    ist7: u64 = 0,
-    reserved_4: u32 = 0,
-    reserved_5: u32 = 0,
-    reserved_6: u16 = 0,
-    io_base: u16 = 0,
+pub const TSS = extern struct {
+    reserved_1: u32 align(1) = 0,
+    rsp: [3]u64 align(1) = .{ 0, 0, 0 },
+    reserved_2: u32 align(1) = 0,
+    reserved_3: u32 align(1) = 0,
+    ist: [7]u64 align(1) = .{ 0, 0, 0, 0, 0, 0, 0 },
+    reserved_4: u32 align(1) = 0,
+    reserved_5: u32 align(1) = 0,
+    reserved_6: u16 align(1) = 0,
+    iopb_offset: u16 align(1) = 0,
 };
 
-const GDTR = packed struct {
+const GDTR = extern struct {
+    limit: u16 align(1),
+    base: u64 align(1),
+};
+
+const GDTEntry = extern struct {
     limit: u16,
-    base: u64,
-};
-
-const GDTEntry = packed struct {
-    limit_1: u16,
     base_1: u16,
     base_2: u8,
     access: u8,
-    flags: u4,
-    limit_2: u4,
+    granularity: u8,
     base_3: u8,
 
-    pub fn make(base: u32, limit: u20, access: u8, flags: u4) GDTEntry {
+    pub fn make(base: u32, limit: u16, access: u8, granularity: u8) GDTEntry {
         return .{
             .base_1 = @truncate(base),
             .base_2 = @truncate(base >> 16),
             .base_3 = @truncate(base >> 24),
-            .limit_1 = @truncate(limit),
-            .limit_2 = @truncate(limit >> 16),
+            .limit = limit,
             .access = access,
-            .flags = flags,
+            .granularity = granularity,
         };
     }
 };
@@ -102,21 +90,16 @@ const GDTEntry = packed struct {
 pub const GDT = struct {
     entries: [7]u64 align(8) = .{
         0, // null
-        @as(u64, @bitCast(GDTEntry.make(0, 0xfffff, kernel_code_access, code_flags))),
-        @as(u64, @bitCast(GDTEntry.make(0, 0xfffff, kernel_data_access, data_flags))),
-        @as(u64, @bitCast(GDTEntry.make(0, 0xfffff, user_code_access, code_flags))),
-        @as(u64, @bitCast(GDTEntry.make(0, 0xfffff, user_data_access, data_flags))),
+        @as(u64, @bitCast(GDTEntry.make(0, 0xffff, kernel_code_access, code_flags))),
+        @as(u64, @bitCast(GDTEntry.make(0, 0xffff, kernel_data_access, data_flags))),
+        @as(u64, @bitCast(GDTEntry.make(0, 0xffff, user_code_access, code_flags))),
+        @as(u64, @bitCast(GDTEntry.make(0, 0xffff, user_data_access, data_flags))),
         0, // TSS low
         0, // TSS high
     },
 
     pub fn load(self: *GDT, tss: *const TSS) void {
-        const tss_entry = TSSEntry.make(
-            @intFromPtr(tss),
-            @sizeOf(TSS) - 1,
-            tss_access,
-            0,
-        );
+        const tss_entry = TSSEntry.make(@intFromPtr(tss), tss_access);
 
         const tss_entry_bits: [2]u64 = @bitCast(tss_entry);
         self.entries[5] = tss_entry_bits[0];
@@ -137,7 +120,7 @@ pub const GDT = struct {
         );
 
         debug.println("[GDT] Reload selectors");
-        flush();
+        // flush();
     }
 
     /// Replaces the selectors set by the bootloader
@@ -168,25 +151,23 @@ test "TSS entry construction" {
         .base_2 = 0x80,
         .base_3 = 0x80,
         .base_4 = 0x8000,
-        .limit_1 = 0x8000,
-        .limit_2 = 0x8,
+        .limit = 0x8000,
         .access = 0,
-        .flags = 0,
+        .granularity = 0,
         .reserved = 0,
     };
     try std.testing.expect(std.meta.eql(value, expected));
 }
 
 test "GDT entry construction" {
-    const value = GDTEntry.make(0x80808000, 0x88000, 0, 0);
+    const value = GDTEntry.make(0x80808000, 0x8000, 0, 0);
     const expected = GDTEntry {
         .base_1 = 0x8000,
         .base_2 = 0x80,
         .base_3 = 0x80,
-        .limit_1 = 0x8000,
-        .limit_2 = 0x8,
+        .limit = 0x8000,
         .access = 0,
-        .flags = 0,
+        .granularity = 0,
     };
     try std.testing.expect(std.meta.eql(value, expected));
 }
