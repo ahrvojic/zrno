62f7771cee4e32ffe376c3f85760efc906636078
Anthony Hrvojic <anth@dezoris.net>
2023-12-02 13:17:20 -0500

    Fix lapic id register

--- kernel/src/dev/ps2.zig
+++ kernel/src/dev/ps2.zig
@@ -4,15 +4,16 @@ const debug = @import("../sys/debug.zig");
 const interrupts = @import("../sys/interrupts.zig");
 const port = @import("../sys/port.zig");
 
+const data_port = 0x60;
+
 pub fn init() !void {
-    // TODO
-    apic.get().routeIrq(cpu.get().lapicId(), interrupts.vec_keyboard, 1);
+    const lapic_id = cpu.get().lapicId();
+    apic.get().routeIrq(lapic_id, interrupts.vec_keyboard, 1);
 }
 
 pub fn handleKeyboardInterrupt() void {
-    const scancode = port.inb(0x60);
+    const scancode = port.inb(data_port);
     debug.print("Scancode: ");
     debug.printInt(scancode);
     debug.println("");
-    // TODO
 }
