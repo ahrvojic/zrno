d739526d8e48b4adcba36f48bce2ba4ac37d3084
Anthony Hrvojic <anth@dezoris.net>
2023-12-10 15:53:34 -0500

    Add keyboard buffer and initial event support

--- kernel/src/dev/ps2.zig
+++ kernel/src/dev/ps2.zig
@@ -4,7 +4,30 @@ const debug = @import("../sys/debug.zig");
 const interrupts = @import("../sys/interrupts.zig");
 const port = @import("../sys/port.zig");
 
-const data_port = 0x60;
+const ps2_data_port = 0x60;
+
+const KeyboardState = enum {
+    normal,
+    prefix,
+};
+
+const KeyModifier = enum {
+    alt,
+    ctrl,
+    shift,
+    super,
+};
+
+const KeyEvent = struct {
+    code: u8,
+    state: u8,
+};
+
+const kb_buffer_size = 256;
+var kb_buffer = [_]?KeyEvent{null} ** kb_buffer_size;
+var kb_buffer_pos: usize = 0;
+
+var kb_state = KeyboardState.normal;
 
 pub fn init() !void {
     const lapic_id = cpu.get().lapicId();
@@ -12,8 +35,30 @@ pub fn init() !void {
 }
 
 pub fn handleKeyboardInterrupt() void {
-    const scancode = port.inb(data_port);
-    debug.print("Scancode: ");
-    debug.printInt(scancode);
+    const code = port.inb(ps2_data_port);
+    debug.print("Scan code: ");
+    debug.printInt(code);
     debug.println("");
+
+    kb_buffer[kb_buffer_pos] = .{
+        .code = code,
+        .state = 0,
+    };
+
+    kb_buffer_pos = (kb_buffer_pos + 1) % kb_buffer_size;
+    debug.print("Keyboard buffer position: ");
+    debug.printInt(kb_buffer_pos);
+    debug.println("");
+
+    updateKeyboardState(code);
+}
+
+fn updateKeyboardState(code: u8) void {
+    if (code == 0xe0) {
+        kb_state = KeyboardState.prefix;
+        debug.println("Keyboard state: prefix");
+    } else if (kb_state == KeyboardState.prefix) {
+        kb_state = KeyboardState.normal;
+        debug.println("Keyboard state: normal");
+    }
 }
