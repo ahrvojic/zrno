64db038e00941996e3066ec1134ffb24e624fa3e
Anthony Hrvojic <anth@dezoris.net>
2023-12-17 09:32:35 -0500

    Add framebuffer module

---
+++ kernel/src/dev/fb.zig
@@ -0,0 +1,12 @@
+const limine = @import("limine");
+
+const debug = @import("../sys/debug.zig");
+
+pub fn init(fb_res: *limine.FramebufferResponse) !void {
+    if (fb_res.framebuffer_count < 1) {
+        debug.panic("No framebuffer available!");
+    }
+
+    const fb = fb_res.framebuffers()[0];
+    _ = fb; // TODO
+}
