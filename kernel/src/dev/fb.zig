e90209242d2cfd521ce64e3f53600a0e3edd039e
Anthony Hrvojic <anth@dezoris.net>
2024-01-13 17:29:40 -0500

    Add tty scrolling

--- kernel/src/dev/fb.zig
+++ kernel/src/dev/fb.zig
@@ -8,33 +8,53 @@ var fb: Framebuffer = .{};
 
 const Framebuffer = struct {
     info: *limine.Framebuffer = undefined,
+    maxRow: usize = 25,
+    maxCol: usize = 80,
 
     pub fn init(self: *@This(), info: *limine.Framebuffer) void {
         self.info = info;
     }
 
-    pub fn plotChar(self: *const @This(), ch: u8, row: u8, col: u8) void {
+    pub fn plotChar(self: *const @This(), ch: u8, row: usize, col: usize) void {
         const glyph = font.builtin.glyph(ch);
 
-        const pxRowStart = row * self.info.pitch * font.builtin.height;
-        const pxColStart = col * self.info.bpp / 8 * font.builtin.width;
+        const rowOffsetStart = self.toRowOffset(row);
+        const colOffsetStart = self.toColOffset(col);
 
-        var pxRow = pxRowStart;
-        var pxCol = pxColStart;
+        var rowOffset = rowOffsetStart;
+        var colOffset = colOffsetStart;
 
         for (glyph) |glyphRow| {
             for (0..font.builtin.width) |i| {
                 if (glyphRow & std.math.shr(u8, 0x80, i) != 0)
                 {
-                    @as(*u32, @ptrCast(@alignCast(self.info.address + pxRow + pxCol))).* = 0xffffffff;
+                    @as(*u32, @ptrCast(@alignCast(self.info.address + rowOffset + colOffset))).* = 0xffffffff;
                 }
 
-                pxCol += self.info.bpp / 8;
+                colOffset += self.info.bpp / 8;
             }
 
-            pxRow += self.info.pitch;
-            pxCol = pxColStart;
+            rowOffset += self.info.pitch;
+            colOffset = colOffsetStart;
         }
+    }
+
+    pub fn scroll(self: *const @This()) void {
+        // Shift framebuffer up one character row
+        const newTop = self.toRowOffset(1);
+        std.mem.copyForwards(u8, self.info.data(), self.info.data()[newTop..]);
+        // Clear last character row
+        for (0..self.maxCol) |col| {
+            self.plotChar(' ', self.maxRow - 1, col);
+        }
+    }
+
+    pub fn toRowOffset(self: *const @This(), row: usize) usize {
+        return row * self.info.pitch * font.builtin.height;
+    }
+
+    pub fn toColOffset(self: *const @This(), col: usize) usize {
+        return col * self.info.bpp / 8 * font.builtin.width;
     }
 };
 
