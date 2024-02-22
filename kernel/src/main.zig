40af65e2cf09d4bae1239295d65e22598aae0228
Anthony Hrvojic <anth@dezoris.net>
2024-02-21 20:21:48 -0500

    Add PMM module

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -7,6 +7,7 @@ const fb = @import("dev/fb.zig");
 const pit = @import("dev/pit.zig");
 const ps2 = @import("dev/ps2.zig");
 const tty = @import("dev/tty.zig");
+const pmm = @import("mm/pmm.zig");
 const cpu = @import("sys/cpu.zig");
 const debug = @import("sys/debug.zig");
 
@@ -15,6 +16,7 @@ pub export var base_revision: limine.BaseRevision = .{ .revision = 1 };
 pub export var bootloader_req: limine.BootloaderInfoRequest = .{};
 pub export var fb_req: limine.FramebufferRequest = .{};
 pub export var hhdm_req: limine.HhdmRequest = .{};
+pub export var mm_req: limine.MemoryMapRequest = .{};
 pub export var rsdp_req: limine.RsdpRequest = .{};
 
 export fn _start() callconv(.C) noreturn {
@@ -34,6 +36,7 @@ pub fn main() !void {
     const bootloader_res = bootloader_req.response.?;
     const fb_res = fb_req.response.?;
     const hhdm_res = hhdm_req.response.?;
+    const mm_res = mm_req.response.?;
     const rsdp_res = rsdp_req.response.?;
 
     const bootloader_name = std.mem.span(bootloader_res.name);
@@ -45,6 +48,9 @@ pub fn main() !void {
 
     debug.println("[Main] Init CPU");
     try cpu.init(hhdm_res);
+
+    debug.println("[Main] Init PMM");
+    try pmm.init(hhdm_res, mm_res);
 
     debug.println("[Main] Init ACPI");
     try acpi.init(hhdm_res, rsdp_res);
