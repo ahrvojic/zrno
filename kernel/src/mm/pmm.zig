bd3b93aa1f59c5198e27550c154c471ff393b3b5
Anthony Hrvojic <anth@dezoris.net>
2024-02-25 20:54:29 -0500

    Enumerate memory map entries

--- kernel/src/mm/pmm.zig
+++ kernel/src/mm/pmm.zig
@@ -1,7 +1,36 @@
+const std = @import("std");
 const limine = @import("limine");
 
+const debug = @import("../sys/debug.zig");
+
+const page_size = 4096;
+
+var highest_phys_addr: u64 = 0;
+
+var usable_pages: usize = undefined;
+var reserved_pages: usize = undefined;
+var bad_pages: usize = undefined;
+
 pub fn init(hhdm_res: *limine.HhdmResponse, mm_res: *limine.MemoryMapResponse) !void {
-    // TODO
-    _ = hhdm_res;
-    _ = mm_res;
+    _ = hhdm_res; // TODO
+
+    for (mm_res.entries()) |entry| {
+        switch (entry.kind) {
+            .usable => {
+                usable_pages += try std.math.divCeil(u64, entry.length, page_size);
+                highest_phys_addr = @max(highest_phys_addr, entry.base + entry.length);
+            },
+            .reserved,
+            .acpi_reclaimable,
+            .acpi_nvs,
+            .bootloader_reclaimable,
+            .kernel_and_modules,
+            .framebuffer => {
+                reserved_pages += try std.math.divCeil(u64, entry.length, page_size);
+            },
+            .bad_memory => {
+                bad_pages += try std.math.divCeil(u64, entry.length, page_size);
+            }
+        }
+    }
 }
