4ef2f1b3769f73c441e815dcf2a2a26751fda376
Anthony Hrvojic <anth@dezoris.net>
2024-02-26 16:17:49 -0500

    Display kernel panic message on tty

--- kernel/src/lib/panic.zig
+++ kernel/src/lib/panic.zig
@@ -3,9 +3,14 @@ const logger = std.log.scoped(.panic);
 const std = @import("std");
 
 const cpu = @import("../sys/cpu.zig");
+const tty = @import("../dev/tty.zig");
 
 pub fn panic(comptime message: []const u8) noreturn {
     cpu.interruptsDisable();
-    logger.err("KERNEL PANIC: {s}", .{message});
+
+    const msg = "KERNEL PANIC: {s}";
+    logger.err(msg, .{message});
+    tty.print(msg ++ "\n", .{message});
+
     cpu.halt();
 }
