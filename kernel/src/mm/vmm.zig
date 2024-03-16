652925718063a9cac6f8715544548f1508738854
Anthony Hrvojic <anth@dezoris.net>
2024-03-16 14:06:47 -0400

    Allocate L3 tables for higher-half memory

--- kernel/src/mm/vmm.zig
+++ kernel/src/mm/vmm.zig
@@ -37,10 +37,35 @@ const PageTable = extern struct {
     entries: [512]PageTableEntry,
 
     pub fn mapPage(self: *@This(), virt_addr: u64, phys_addr: u64, flags: u64) !void {
-        _ = self;
-        _ = virt_addr;
-        _ = phys_addr;
-        _ = flags;
+        // Extract page table indexes from virtual address
+        const pml4_idx = @as(usize, virt_addr >> 39) & 0x1ff;
+        const pml3_idx = @as(usize, virt_addr >> 30) & 0x1ff;
+        const pml2_idx = @as(usize, virt_addr >> 21) & 0x1ff;
+        const pml1_idx = @as(usize, virt_addr >> 12) & 0x1ff;
+
+        // Walk page table hierarchy
+        const pml3 = self.getNextLevel(pml4_idx, true) orelse return error.OutOfMemory;
+        const pml2 = pml3.getNextLevel(pml3_idx, true) orelse return error.OutOfMemory;
+        const pml1 = pml2.getNextLevel(pml2_idx, true) orelse return error.OutOfMemory;
+        const entry = &pml1.entries[pml1_idx];
+
+        entry.setAddress(phys_addr);
+        entry.setFlags(flags);
+    }
+
+    fn getNextLevel(self: *@This(), index: usize, allocate: bool) ?*PageTable {
+        var entry = &self.entries[index];
+
+        if (entry.getFlags() & Flags.Present != 0) {
+            return virt.toHH(*PageTable, entry.getAddress());
+        } else if (allocate) {
+            const next_level = pmm.alloc(1) orelse return null;
+            entry.setAddress(next_level);
+            entry.setFlags(Flags.Present | Flags.Writable | Flags.User);
+            return virt.toHH(*PageTable, next_level);
+        }
+
+        return null;
     }
 };
 
@@ -52,10 +77,13 @@ pub const VMObject = struct {
 
 pub fn init() !void {
     const pt_addr_phys = pmm.alloc(1) orelse return error.OutOfMemory;
-    const page_table = virt.toHH(*PageTable, pt_addr_phys);
-    _ = page_table;
+    const pt = virt.toHH(*PageTable, pt_addr_phys);
 
-    // TODO: Populate higher-half address space in page table
+    // Allocate L3 tables for higher-half memory
+    for (256..512) |i| {
+        _ = pt.getNextLevel(i, true);
+    }
+
     // TODO: Map text, rodata, and data sections
     // TODO: Identity-map physical memory
     // TODO: Higher-half-map physical memory
