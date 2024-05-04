7d5a4ff3cd3c3bedcad865e6dcf8517f1afc6581
Anthony Hrvojic <anth@dezoris.net>
2024-05-04 12:43:14 -0400

    Add heap allocator struct

--- kernel/src/mm/heap.zig
+++ kernel/src/mm/heap.zig
@@ -4,6 +4,30 @@ const std = @import("std");
 
 const vmm = @import("vmm");
 
+pub const HeapAllocator = struct {
+    pub fn alloc(self: *@This(), len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
+        _ = self;
+        _ = len;
+        _ = ptr_align;
+        _ = ret_addr;
+    }
+
+    pub fn resize(self: *@This(), buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+        _ = self;
+        _ = buf;
+        _ = buf_align;
+        _ = new_len;
+        _ = ret_addr;
+    }
+
+    pub fn free(self: *@This(), buf: []u8, buf_align: u8, ret_addr: usize) void {
+        _ = self;
+        _ = buf;
+        _ = buf_align;
+        _ = ret_addr;
+    }
+};
+
 pub fn init() !void {
-    // TODO: Create kernel heap
+    // Do nothing
 }
