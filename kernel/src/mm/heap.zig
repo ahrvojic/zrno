6299555bc46a4526fd68f305b81f67d43f5deae7
Anthony Hrvojic <anth@dezoris.net>
2024-05-05 22:59:33 -0400

    Add heap allocator init

--- kernel/src/mm/heap.zig
+++ kernel/src/mm/heap.zig
@@ -2,9 +2,22 @@ const logger = std.log.scoped(.heap);
 
 const std = @import("std");
 
+const pmm = @import("pmm");
 const vmm = @import("vmm");
 
 pub const HeapAllocator = struct {
+    heap_vmm: *vmm.VMM = undefined,
+    heap_start_addr: u64 = undefined,
+    heap_end_addr: u64 = undefined,
+    zeros_phys_addr: u64 = undefined,
+
+    pub fn init(self: *@This(), heap_vmm: *vmm.VMM, start_addr: u64, size: usize) !void {
+        self.heap_vmm = heap_vmm;
+        self.heap_start_addr = std.mem.alignBackward(u64, start_addr, pmm.page_size);
+        self.heap_end_addr = std.mem.alignForward(u64, start_addr + size, pmm.page_size);
+        self.zeros_phys_addr = pmm.alloc(1) orelse return error.OutOfMemory;
+    }
+
     pub fn allocator(self: *@This()) std.mem.Allocator {
         return .{
             .ptr = self,
