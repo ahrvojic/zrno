4e91b849d9f6abc6c752a3ecdb893331d849f671
Anthony Hrvojic <anth@dezoris.net>
2024-10-21 22:50:08 -0400

    Fix heap allocator

--- kernel/src/sched/proc.zig
+++ kernel/src/sched/proc.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 
 const cpu = @import("../sys/cpu.zig");
-const vmm = @import("../mm/vmm.zig");
 
 pub const Process = struct {
     pid: u64,
-    threads: std.DoublyLinkedList(*Thread),
-    vmm: *vmm.VMM,
+    heap: std.mem.Allocator,
+    threads: std.DoublyLinkedList(void),
+    node: std.DoublyLinkedList(void).Node,
 };
 
 pub const Thread = struct {
     tid: u64,
     parent: *Process,
     ctx: cpu.Context = std.mem.zeroes(cpu.Context),
+    node: std.DoublyLinkedList(void).Node,
 };
